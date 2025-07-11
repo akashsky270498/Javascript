e29464e62b22f655dff8966e37997eac66ea78db
Akash <akashsky270498@gmail.com>
2025-07-11 11:20:27 +0530

    Dialog Boxes in JS [DOME]

---
+++ 15-Dialog Boxes/script.js
@@ -0,0 +1,10 @@
+alert("Be alert");
+const isConfirmed = confirm("Do you want to proceed now?");
+console.log("isConfirmed: ", isConfirmed);
+prompt("Please! Enter your query?");
+
+/*
+Alert:- Alert will give you alert message with ok button.
+Confirm:- Confirm will give msg with two buttons.
+Propmt:- Prompt will give u input box wih one button.
+*/
