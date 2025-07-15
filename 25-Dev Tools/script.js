3c85d3cba3aeda63faab3d29271dd7a89eb2fcd3
Akash <akashsky270498@gmail.com>
2025-07-15 11:49:00 +0530

    Dev tools in JS [DONE]

---
+++ 25-Dev Tools/script.js
@@ -0,0 +1,12 @@
+const firstName = "Nishant";
+const lastName = "Sharma";
+const isLoggedIn = false;
+const isGraduated = false;
+const userAge = 26;
+const hasJob = true;
+const myNull = null;
+const myUndefined = undefined;
+const firstName2 = "Nish" + "ant";
+const emptyStr = "";
+
+//Go to dev console -> memory -> takeSnapshot
