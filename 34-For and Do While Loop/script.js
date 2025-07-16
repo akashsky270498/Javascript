00e0de086951c230e08db93badf17428d78edfd0
Akash <akashsky270498@gmail.com>
2025-07-16 17:58:32 +0530

    For and do while loop in JS [DONE]

---
+++ 34-For and Do While Loop/script.js
@@ -0,0 +1,25 @@
+const users = [
+  "Akash",
+  "Rahul",
+  "Adarsh",
+  "Arif",
+  "Gaurav",
+  "Anurag",
+  "Rakesh",
+];
+
+for (let i = 0; i < users.length; i++) {
+  console.log(`${i + 1}. ${users[i]} procodrr`);
+}
+
+for (let i = 0; i <= 100; i++) {
+  if (i !== 0 && i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
