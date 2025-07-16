6b6f02feb3627f73b3063cd8b6af4ca255e87534
Akash <akashsky270498@gmail.com>
2025-07-16 17:35:21 +0530

    While loop in JS [DONE]

---
+++ 33-While Loop/script.js
@@ -0,0 +1,21 @@
+let num = 1;
+while (num <= 100) {
+  console.log(num);
+  num++;
+}
+
+const users = [
+  "Akash",
+  "Rahul",
+  "Adarsh",
+  "Arif",
+  "Gaurav",
+  "Anurag",
+  "Rakesh",
+];
+
+let i = 0;
+while (i < users.length) {
+  console.log(`${i + 1}. ${users[i]} procodrr`);
+  i++;
+}
