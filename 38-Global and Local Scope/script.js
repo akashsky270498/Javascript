4c62aedb3b09b66208c7b4d5960510d29ad1afa1
Akash <akashsky270498@gmail.com>
2025-07-17 17:38:28 +0530

    Global and Local scope in JS [DONE]

---
+++ 38-Global and Local Scope/script.js
@@ -0,0 +1,23 @@
+const username = "Anurag";
+let userAge = 23;
+var a = 50;
+
+function add() {
+  const username = "Sameer";
+  const x = 23;
+  const y = 12;
+  console.log(x + y);
+  console.log(username); // Sameer
+}
+
+function subtract() {
+  const x = 23;
+  y = 11;
+  console.log(x - y);
+  console.log(username); // "Anurag"
+}
+
+add();
+subtract();
+
+console.log("Program ends");
