1267eeeecfec7ec0c8034055687512bf1838c78d
Akash <akashsky270498@gmail.com>
2025-07-17 18:22:13 +0530

    Lexical scope and Block scope

--- 38-Global and Local Scope/script.js
+++ 38-Global and Local Scope/script.js
@@ -21,3 +21,37 @@ add();
 subtract();
 
 console.log("Program ends");
+
+// Block and Lexical scope
+
+function parent() {
+  const x = 15;
+  const y = 20;
+  const z = 32;
+
+  function child() {
+    const childName = "Golu";
+    console.log("childName: ", childName);
+    console.log("Z from parent: ", z);
+
+    if (true) {
+      let c = 20;
+      var d = 22;
+      console.log("C: ", c);
+      console.log("D: ", d);
+    }
+    console.log("D: ", d); // 22 bcoz of var (this is block scope)
+
+    function grandChild() {
+      const grandChildName = "Molu";
+      console.log("Grand Child Name: ", grandChildName);
+      console.log("username from global: ", username);
+    }
+    grandChild();
+  }
+  child();
+}
+
+parent();
+
+// Note: parent & child are lexical scope for grandChild
