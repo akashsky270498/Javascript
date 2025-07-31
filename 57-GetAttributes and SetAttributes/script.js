6c8d47fb7c1e11fb3b7f297c404ca812b32ac12b
Akash <akashsky270498@gmail.com>
2025-07-31 13:01:21 +0530

    Set and Get Attributes in JS [DONE]

---
+++ 57-GetAttributes and SetAttributes/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".css-image").setAttribute("title", "no title");
+document.querySelector(".css-image").setAttribute("title", "no title");
+document.querySelector("h2").id; //this way also we can access the id attribute as it is a predefined attributes.
+document.querySelector("h2").id = "heading"; //this way also we can set the id attribute as it is a predefined attributes.
