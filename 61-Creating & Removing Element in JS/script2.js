ddb3530b9ae923a002f60eef170bee0a87922108
Akash <akashsky270498@gmail.com>
2025-08-02 16:27:07 +0530

    Creating and Reoving Element in JS [DONE]

--- 61-Creating & Removing Element in JS/script2.js
+++ 61-Creating & Removing Element in JS/script2.js
@@ -26,13 +26,24 @@ const container = document.querySelector(".container");
 
 let myHTML = ``;
 
-for (let i = 1; i  <= 100; i++) {
-    myHTML += `
+for (let i = 1; i <= 100; i++) {
+  myHTML += `
     <div class="img-container"> 
     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png">
     <p>${i}</p>
     </div>
-    `
+    `;
 }
 
-container.innerHTML = myHTML
+container.innerHTML = myHTML;
+
+//
+let itemToDelete = document.querySelector("body > div > div:nth-child(4)");
+itemToDelete.remove() || itemToDelete.parentElement.removeChild(itemToDelete);
+itemToDelete = null;
+
+
+/*
+Either dont save the reference to the element you want to delete or set it to null after removing it.
+This helps in garbage collection, allowing the browser to reclaim memory.
+*/
