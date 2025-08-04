9f43ecc90848f40d9239b76d6d30a0996605f4aa
Akash <akashsky270498@gmail.com>
2025-08-04 13:29:59 +0530

    Events Simulation in JS [DONE]

---
+++ 67-Event Simulation/script.js
@@ -0,0 +1,43 @@
+const addCardButton = document.querySelector(".card");
+const container = document.querySelector(".container");
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+let count = 1;
+
+addCardButton.addEventListener("click", () => {
+  const newCard = document.createElement("div");
+  newCard.classList.add("card");
+  newCard.innerText = count++;
+  container.append(newCard);
+});
+
+// const intervalId = setInterval(() => {
+//   if (count >= 10) {
+//     clearInterval(intervalId);
+//   }
+
+//   addCardButton.click();
+// }, 50);
+
+// Input Focus & Blurred
+// setTimeout(() => {
+//   input.focus();
+//   console.log("Input Focused");
+// }, 4000);
+
+// setTimeout(() => {
+//   input.blur();
+//   console.log("Input Blurred");
+// }, 6000);
+
+//Form
+// setTimeout(() => {
+//   form.submit();
+//   console.log("Form Subbmitted");
+// }, 4000);
+
+//Form Reset
+setTimeout(() => {
+  form.reset();
+  console.log("Form Resetted.");
+},6000);
