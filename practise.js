cf27c09dc364a9dc15116b55cb06f884a94996a1
Akash <akashsky270498@gmail.com>
2026-03-31 18:09:03 +0530

    added: PD

--- practise.js
+++ practise.js
@@ -1,19 +1,18 @@
-const moveZeros = (nums) => {
-  let x = 0;
+const secondLargest = (nums) => {
+  if (!Array.isArray(nums)) return false;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[x] = nums[i];
-      x++;
+  let max = -Infinity;
+  let secondMax = -Infinity;
+  for (let num of nums) {
+    if (num > max) {
+      secondMax = max;
+      max = num;
+    } else if (num < max && num > secondMax) {
+      secondMax = num;
     }
   }
-
-  for (let i = x; i < nums.length; i++) {
-    nums[i] = 0;
-  }
-  return nums;
+  return secondMax === -Infinity ? null : secondMax;
 };
 
-console.log(moveZeros([0, 1, 0, 3, 12])); // [1,3,12,0,0]
-console.log(moveZeros([0])); // [0]
-console.log(moveZeros([]));
+console.log(secondLargest([10, 5, 8, 200, 15, 35]));
+console.log(secondLargest([10]));
